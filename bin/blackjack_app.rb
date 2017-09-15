4bfa00c8fae259f1397d1578457391e866f3a87d
gingin77 <ginniehench@gmail.com>
2017-09-15 19:20:48 -0400

    made changes to the bj_app file

--- bin/blackjack_app.rb
+++ bin/blackjack_app.rb
@@ -1,23 +1,10 @@
-#!/usr/bin/env ruby
-
-require_relative '../lib/pig'
+require_relative '../lib/game'
+require_relative '../lib/hand'
+require_relative '../lib/deck'
 require_relative '../lib/player'
+require_relative '../lib/dealer'
 
-players = []
+cards = []??
 
-ARGV.each do |arg|
-  player_class = arg
-  if defined?(player_class)
-    players.push(Module.const_get(player_class).new(arg.gsub(/Player$/, "")))
-  end
-end
-
-if players.length < 2
-  players.push(CautiousPlayer.new("Cautious1"))
-end
-if players.length == 1
-  players.push(CautiousPlayer.new("Cautious2"))
-end
-
-pig = Pig.new(*players, games: 100, silent: true)
-pig.run
+game = game.new()
+game.run
