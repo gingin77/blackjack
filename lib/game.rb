161e31b0df9c34124212d5d3b1887eeba9cb7818
gingin77 <ginniehench@gmail.com>
2017-09-16 15:39:04 -0400

    I'm trying to figure out the sequnce of actions and variable declarations. at present, when I run the app, I'm gettig an error that states, h' for main:Object (NameError)

--- lib/game.rb
+++ lib/game.rb
@@ -1,23 +1,39 @@
-class game
-  def initialize(hand: 1, silent: false)
-    # @deck
-    # @dealer = dealer
-    # @player = player
-    # @hand
+require 'pry'
+
+class Game
+  def initialize(response, hand: 1)
+    @response = response
+    @hand = hand
+  end
+
+  # def start_display
+  #   # hand.card_sum
+  #   string_hard = ("Hello and welcome to the game of blackjack! Let's begin.
+  #
+  #   You have $100 and bet $10.
+  #
+  #   You have a #{card1} and a #{card2}.
+  #   Your total is #{}"+ stars + "\n")
+  #   print string
+  # end
+
+  def get_hit_or_stand
+    while true
+      print "Do you want to (h)it or (s)tand?  "
+      response = gets.chomp.downcase
+      if @response[0] == "h"
+        return true
+      elsif @response[0] == "s"
+        return false
+      end
+      puts "That is not a valid response!"
+    end
+    dealer_deals
   end
 
 
-
-
-  def start_display
-
-    hand.card_sum
-    string_hard = ("Hello and welcome to the game of blackjack! Let's begin.
-
-    You have $100 and bet $10.
-
-    You have a #{card1} and a #{card2}.
-    Your total is #{}"+ stars + "\n")
-    print string
+  def dealer_deals(*response)
+    print "Because you said #{response}, the dealer will now give you a new card."
   end
+  # binding.pry
 end
