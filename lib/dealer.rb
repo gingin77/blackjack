b9e52a6c3898b4c7221d7c429d218200eeb93796
gingin77 <ginniehench@gmail.com>
2017-09-19 21:23:48 -0400

    under the current structure, the 'top' 2 cards, ie index positions 50 and 51, are the cards that go to the players hand. the next 2 go to the dealer

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -7,8 +7,7 @@ class Dealer
 
   def initialize
     @shuffled_deck = Deck.new.create_deck
-    # @card_deck = deck.card_deck
-    puts @shuffled_deck[-4..51]
+    puts @shuffled_deck[-2..51]
     puts "^^ shuffled_deck"
     @players_hand = deal_to_player
     @dealers_hand = deal_to_dealer
@@ -20,7 +19,6 @@ class Dealer
     players_hand = @shuffled_deck.pop(2)
     puts players_hand
     puts "/n ^^players_hand"
-    # binding.pry
   end
 
   def deal_to_dealer
@@ -29,7 +27,6 @@ class Dealer
     puts dealers_hand
     puts "/n ^^dealers_hand"
   end
-
 end
 
     # Each time a random_card is used to generate an integer, that number needs to be noted... the size of the deck array will go down as cards are picked. an iteration loop AND a reducer of some kind are needed.
