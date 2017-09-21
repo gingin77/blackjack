4aeb452521a2ac98450c79dfd39ce8f9ef3f2122
gingin77 <ginniehench@gmail.com>
2017-09-21 01:45:22 -0400

    the score function is almost finalized but the ace display method isn't working

--- lib/hand.rb
+++ lib/hand.rb
@@ -9,19 +9,39 @@ class Hand
   def initialize(hand1, hand2)
     @players_hand = hand1
     @dealers_hand = hand2
-    concatenate(@players_hand, @dealers_hand)
-    print face_convert(@players_hand)
+    score_status(@players_hand, @dealers_hand)
   end
 
-  def show_two_cards(hand)
-    " a #{hand[0].rank} of #{hand[0].suit} and a #{hand[1].rank} of #{hand[1].suit}"
+  def score(hand)
+    message = ""
+    if any_ace?(hand) == true
+      has_ace(hand)
+    else
+      message = "Your hand is worth #{face_convert(hand).sum} points."
+    end
+    message
   end
 
-  def show_one_card(hand)
-    " a #{hand[0].rank} of #{hand[0].suit}"
+  def has_ace(hand)
+    message = ""
+    if handle_ace(hand).count(true) == 1
+      message = "You have a soft hand, with an Ace that is worth 11 or 1."
+    else
+      message = "You have 2 Aces, with a soft score of 12"
+    end
+    message
   end
 
-# hand.map { |rank, suit| (rank == :Jack || rank == :Queen || rank == :King) ? "10" }
+  def handle_ace(hand)
+    hand_get_ranks(hand).map! do |rank|
+      rank == :Ace ? true : false
+    end
+  end
+
+  def any_ace?(hand)
+    handle_ace(hand).any?
+  end
+
   def hand_get_ranks(hand)
     ranks = []
     ranks.push(hand[0].rank, hand[1].rank)
@@ -34,19 +54,21 @@ class Hand
     end
   end
 
-  def handle_ace(hand)
-    hand_get_ranks(hand).map! do |rank|
-      rank == :Jack ? true : false
-    end
+  def show_two_cards(hand)
+    " a #{hand[0].rank} of #{hand[0].suit} and a #{hand[1].rank} of #{hand[1].suit}"
   end
 
+  def show_one_card(hand)
+    " a #{hand[0].rank} of #{hand[0].suit}"
+  end
 
   # def ace_handler
   #   if hand[0].rank != :Ace && hand[1].rank != :Ace
   # end
 
-  def concatenate(players_hand, dealers_hand)
-    print "You have" + show_two_cards(players_hand) + ". The dealer has one card face down is showing" + show_one_card(dealers_hand) + ".
+  def score_status(players_hand, dealers_hand)
+    print "You have" + show_two_cards(players_hand) + ". " + score(players_hand) + "
+    The dealer has one card face down is showing" + show_one_card(dealers_hand) + ".
 
     "
   end
